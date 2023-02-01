de195ad06657a757b87bbb2f0a941b069892ea18
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-02 02:26:02 +0600

    Problem solving 01 complete presented programming hero for beginer

---
+++ problem-solving-01.js
@@ -0,0 +1,6 @@
+// Harry’s mom gave him money and asked him to buy some oranges and apples. Write a program to help Harry calculate how much money the shopkeeper will return.
+
+let giveMoneyMom = 1000;
+let applePrice = 700;
+let giveMeShowkeaper = giveMoneyMom - applePrice;
+console.log(giveMeShowkeaper);
