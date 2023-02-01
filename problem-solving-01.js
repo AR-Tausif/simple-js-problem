974b1940b055d813671311d38bbdb22b720722b9
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-02 02:30:49 +0600

    Problem solving 02 complete presented by programming hero for beginer

--- problem-solving-01.js
+++ problem-solving-01.js
@@ -4,3 +4,15 @@ let giveMoneyMom = 1000;
 let applePrice = 700;
 let giveMeShowkeaper = giveMoneyMom - applePrice;
 console.log(giveMeShowkeaper);
+
+
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+let math = 75.25,
+    biology = 65,
+    chemistry = 80,
+    physics = 35.45,
+    bangla = 99.50;
+let allResult = (math + biology + chemistry + physics + bangla) / 5;
+
+console.log(allResult.toFixed(2));
